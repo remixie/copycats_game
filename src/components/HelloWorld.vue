94b8e7af1f0c779ab0db370e0d8d2dc5303f2701
Rem <remixielive@live.com>
2021-12-26 15:08:20 -0500

    Connect to Solana Blockchain

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,80 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    <div v-if="account != ''">
+      <h3>Your wallet address is: {{ account }}</h3>
+      <div class="cat-selector">
+        Select a CopyCat:
+        <table>
+          <tr v-for="cc in copycats_metadata" :key="cc.mint">
+            <td>{{ cc.mint }}</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <p v-else>
+      <button @click="connectToWallet()">Connect to Phantom Wallet</button>
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
 <script>
+import { Connection } from "@solana/web3.js";
+import {
+  getParsedNftAccountsByOwner,
+  //isValidSolanaAddress,
+} from "@nfteyez/sol-rayz";
+const connection = new Connection("https://api.mainnet-beta.solana.com");
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  data: () => {
+    return { account: "", error: [], copycats_metadata: [] };
+  },
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  methods: {
+    async connectToWallet() {
+      try {
+        const resp = await window.solana.connect();
+        this.account = resp.publicKey.toString();
+        //console.log(resp);
+
+        let ownerToken = this.account;
+        //const result = isValidSolanaAddress(ownerToken);
+        //console.log("result", result);
+        const nfts = await getParsedNftAccountsByOwner({
+          publicAddress: ownerToken,
+          connection,
+          serialization: true,
+        });
+        //return nfts;
+        //console.log(nfts);
+
+        let copycat_nfts = nfts.filter(
+          (cat) =>
+            cat.updateAuthority ===
+            "9mmdJRBi9zU5t4n633TzMEGyXnRjNEEyogV98uCNH7GD"
+        );
+
+        console.log(copycat_nfts);
+
+        this.copycats_metadata = copycat_nfts;
+      } catch (err) {
+        this.error = { code: 4001, message: "User rejected the request." };
+      }
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.cat-selector {
+  margin: 3rem;
+}
+
 h3 {
   margin: 40px 0 0;
 }
