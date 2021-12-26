2b57b0c5439bb7870c78ec9ac93ce720768c29cf
Rem <remixielive@live.com>
2021-12-26 17:39:12 -0500

    Add cat selection and moonrank data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,37 @@
 <template>
-  <div>
+  <div class="gameplay_area">
     <h1>{{ msg }}</h1>
     <div v-if="account != ''">
-      <h3>Your wallet address is: {{ account }}</h3>
-      <div class="cat-selector">
+      <p>Your Address: {{ account }}</p>
+      <div v-if="selected_cat.length == 0" class="cat-selector">
         Select a CopyCat:
         <table>
-          <tr v-for="cc in copycats_metadata" :key="cc.mint">
-            <td>{{ cc.mint }}</td>
+          <tr>
+            <td
+              v-for="cc in copycats_metadata"
+              :key="cc.mint"
+              @click="select(cc)"
+            >
+              <img :src="cc.image" />
+            </td>
           </tr>
         </table>
       </div>
+      <div v-else>
+        <h2>You selected {{ selected_cat.name.replace("CopyCats ", "") }}</h2>
+        Rarity: Type:
+        {{ selected_cat.attributes[1].value }}
+      </div>
     </div>
     <p v-else>
-      <button @click="connectToWallet()">Connect to Phantom Wallet</button>
+      <button @click="connectToWallet()">Connect</button>
     </p>
   </div>
 </template>
 
 <script>
 import { Connection } from "@solana/web3.js";
+import axios from "axios";
 import {
   getParsedNftAccountsByOwner,
   //isValidSolanaAddress,
@@ -29,12 +41,20 @@ const connection = new Connection("https://api.mainnet-beta.solana.com");
 export default {
   name: "HelloWorld",
   data: () => {
-    return { account: "", error: [], copycats_metadata: [] };
+    return { account: "", error: [], copycats_metadata: [], selected_cat: [] };
   },
   props: {
     msg: String,
   },
   methods: {
+    select(cat_obj) {
+      console.log(cat_obj);
+      this.selected_cat = {
+        mint: cat_obj.mint,
+        attributes: cat_obj.attributes,
+        name: cat_obj.name,
+      };
+    },
     async connectToWallet() {
       try {
         const resp = await window.solana.connect();
@@ -58,9 +78,23 @@ export default {
             "9mmdJRBi9zU5t4n633TzMEGyXnRjNEEyogV98uCNH7GD"
         );
 
-        console.log(copycat_nfts);
+        //console.log(copycat_nfts);
 
-        this.copycats_metadata = copycat_nfts;
+        let arr = [];
+        let n = copycat_nfts.length;
+        for (let i = 0; i < n; i++) {
+          //console.log(copycat_nfts[i].data.uri);
+          let val = await axios.get(copycat_nfts[i].data.uri);
+          arr.push({
+            mint: copycat_nfts[i].mint,
+            image: val.data.image,
+            attributes: val.data.attributes,
+            name: val.data.name,
+          });
+        }
+        this.copycats_metadata = arr;
+
+        //this.copycats_metadata = copycat_nfts;
       } catch (err) {
         this.error = { code: 4001, message: "User rejected the request." };
       }
@@ -74,7 +108,16 @@ export default {
 .cat-selector {
   margin: 3rem;
 }
-
+h1 {
+  margin: 0;
+  padding-top: 10px;
+}
+img {
+  cursor: pointer;
+  width: 100px;
+  height: 100px;
+  border-radius: 15px;
+}
 h3 {
   margin: 40px 0 0;
 }
